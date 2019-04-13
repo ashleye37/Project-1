04ee0d3a8b443fddd00f3a81549f77267fc2ffb4
CBirkbeck <cnbirkbeck@gmail.com>
2019-04-13 15:46:57 -0700

    added questions to html, removed from js, debugged radio btn issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -92,72 +92,11 @@ function _showDecisionDiv() {
 
 // Show questionnaire div
 function _showQuestionnaire() {
-     // array of objects with the questions and possible answers
-     var quizQuestions = [
-        {
-            question: "Which city would you like to travel to?",
-            choices: " ",
-        },
-        {
-            question: "Who will you be traveling with?",
-            choices: [" Solo travel!", "With my gals or guys!", "With my main squeeze!", "Family get away with the kids!"],
-        },
-        {
-            question: "How long would you like your trip to last?",
-            choices: [" Less than 1 week", "1-2 weeks", "2 weeks - 1 month", "More than a month"],
-        },
-        {
-            question: "Are you interested in seeing attraction suggestions?",
-            choices: [" Yes", "No"],
-        },
-        {
-            question: "Are you interested in seeing hotel suggestions?",
-            choices: [" Yes", "No"],
-        },
-        {
-            question: "Are you interested in seeing restaurant suggestions?",
-            choices: [" Yes", "No"],
-        }
 
-    ];
-
-
-    // pull questions from the array of questions, loop through them, and append to UI
-    var questionContainer = $("#questions");
-    questionContainer.append("<h4>Please answer the following questions to help us customize your trip planning experience.</h4>");
-
-    //display question 1 intergrate user input for the city
-    questionContainer.append("<div>"+quizQuestions[0].question+ "</div>"); 
-    questionContainer.append("<div>"+quizQuestions[0].choices[0] + "<div class='input-group mb-3'><input type='text' class='form-control' placeholder='City' aria-label='city' aria-describedby='button-addon2'></div>" + "</div>" );
-    
-    //question 2
-    questionContainer.append("<div>"+ quizQuestions[1].question+ "</div>");
-    questionContainer.append("<div>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[1].choices[0]+ "<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[1].choices[1]+"<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[1].choices[2]+ "<br>"+"<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>" +quizQuestions[1].choices[3]+ "</div");
-    //question 3
-    questionContainer.append("<div>"+ quizQuestions[2].question+ "</div>");
-    questionContainer.append("<div>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[2].choices[0]+ "<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[2].choices[1]+"<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[2].choices[2]+ "<br>"+"<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>" +quizQuestions[2].choices[3]+ "</div");
-    //question 4
-    questionContainer.append("<div>"+ quizQuestions[3].question+ "</div>");
-    questionContainer.append("<div>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[3].choices[0]+ "<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[3].choices[1]+"</div");
-    //question 5     
-    questionContainer.append("<div>"+ quizQuestions[4].question+ "</div>");
-    questionContainer.append("<div>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[4].choices[0]+ "<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[4].choices[1]+"</div");
-    //question 6
-    questionContainer.append("<div>"+ quizQuestions[5].question+ "</div>");
-    questionContainer.append("<div>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[5].choices[0]+ "<br>"+ "<div class='form-check form-check-inline'><input class='form-check-input' type='radio' name='inlineRadioOptions'+i+'' id='radio'+i+'><label class='form-check-label' id + choices '</label></div>"+ quizQuestions[5].choices[1]+"</div");
-   
-    //on submit click, takes user's answers uses data to make api call to get relevant itinerary information. 
-    $("#questionBtn").on("click",function(){
-        $("input type='text'").val();
-        $( "input[type=radio][name=baz]:checked" ).val();
-        return val()
-    })
-
-    console.log('Showing questionnaire')
+   console.log('Showing questionnaire')
 
 };
-//call questions function for testing
-_showQuestionnaire();
+
 
 // Show itinerary div
 function _showItinerary() {
