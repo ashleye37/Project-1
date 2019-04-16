c2e7577f62c6e1ccb85d0763a630941fbfb5e078
Ashley Evans <ashley_e@live.com>
2019-04-15 18:50:52 -0700

    Committing my changes before pulling in new data from master.

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -17,8 +17,7 @@ var restaurantVenueId = [];
 
 $(document).ready(function () {
 
-  //This API query will help us with pulling trending activities or restaurants.
-  //function showHotelsNearLocation(location) {
+  //function showHotelsWeekOrLess(location) {
   //var hotelQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + TristansId + "&client_secret=" + TristansSecret + "&near=" + location + "&query=hotel&v=20190415"
   var hotelQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + cassieClientId + "&client_secret=" + cassieClientSecret + "&near=denver&query=hotel&v=20190415"
 
@@ -26,22 +25,18 @@ $(document).ready(function () {
     url: hotelQueryURL,
     method: "GET"
   }).then(function (response) {
-    //Entire object pulled.
-    console.log(response);
-    //Pulling location to be added to location on trip card.
-    console.log(response.response.geocode.feature.displayName);
     //Pulling hotel IDs for getting pictures for cards.
     var hotel1 = response.response.venues[0].id;
     var hotel2 = response.response.venues[1].id;
     var hotel3 = response.response.venues[2].id;
 
-    hotelVenueId = [
+    hotelVenueId3 = [
       hotel1,
       hotel2,
       hotel3
     ];
 
-    getHotelPic();
+    getHotelPic3();
 
     console.log("Hotel 1 ID: " + response.response.venues[0].id);
     console.log("Hotel 2 ID: " + response.response.venues[1].id);
@@ -56,87 +51,146 @@ $(document).ready(function () {
     $("#hotel2Address").append("Hotel Address: " + response.response.venues[1].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
     //Adding Hotel 3.
     $("#hotel3").append("Hotel Name: " + response.response.venues[2].name);
-    $("#hotel3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
+    $("#hotel3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[2].location.formattedAddress[1]);
   });
 
+  //function showHotelsMoreThanWeek(location) {
+  //var hotelQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + TristansId + "&client_secret=" + TristansSecret + "&near=" + location + "&query=hotel&v=20190415"
+  var hotelQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + cassieClientId + "&client_secret=" + cassieClientSecret + "&near=denver&query=hotel&v=20190415"
+
+  $.ajax({
+    url: hotelQueryURL,
+    method: "GET"
+  }).then(function (response) {
+   
+    //Pulling hotel IDs for getting pictures for cards.
+    var hotel1 = response.response.venues[0].id;
+    var hotel2 = response.response.venues[1].id;
+    var hotel3 = response.response.venues[2].id;
+    var hotel4 = response.response.venues[3].id;
+    var hotel5 = response.response.venues[4].id;
+
+    hotelVenueId5 = [
+      hotel1,
+      hotel2,
+      hotel3,
+      hotel4,
+      hotel5
+    ];
+
+    getHotelPic5();
+
+    console.log("Hotel 1 ID: " + response.response.venues[0].id);
+    console.log("Hotel 2 ID: " + response.response.venues[1].id);
+    console.log("Hotel 3 ID: " + response.response.venues[2].id);
+    console.log("Hotel 4 ID: " + response.response.venues[3].id);
+    console.log("Hotel 5 ID: " + response.response.venues[4].id);
+
+    //Adding information to hotel suggestion cards.
+    //Adding Hotel 1.
+    $("#hotel1").append("Hotel Name: " + response.response.venues[0].name);
+    $("#hotel1Address").append("Hotel Address: " + response.response.venues[0].location.formattedAddress[0] + ", " + response.response.venues[0].location.formattedAddress[1]);
+    //Adding Hotel 2.
+    $("#hotel2").append("Hotel Name: " + response.response.venues[1].name);
+    $("#hotel2Address").append("Hotel Address: " + response.response.venues[1].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
+    //Adding Hotel 3.
+    $("#hotel3").append("Hotel Name: " + response.response.venues[2].name);
+    $("#hotel3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[2].location.formattedAddress[1]);
+    //Adding Hotel 4.
+    $("#hotel3").append("Hotel Name: " + response.response.venues[3].name);
+    $("#hotel3Address").append("Hotel Address: " + response.response.venues[3].location.formattedAddress[0] + ", " + response.response.venues[3].location.formattedAddress[1]);
+    //Adding Hotel 5.
+    $("#hotel3").append("Hotel Name: " + response.response.venues[4].name);
+    $("#hotel3Address").append("Hotel Address: " + response.response.venues[4].location.formattedAddress[0] + ", " + response.response.venues[4].location.formattedAddress[1]);
+  });
+
+  //function showrestaurantsWeekOrLess
   var restaurantQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + cassieClientId + "&client_secret=" + cassieClientSecret + "&near=denver&query=restaurant&v=20190415"
 
   $.ajax({
     url: restaurantQueryURL,
     method: "GET"
   }).then(function (response) {
-    //Entire object pulled.
-
+    //Pulling hotel IDs for getting pictures for cards.
     var restaurant1 = response.response.venues[0].id;
     var restaurant2 = response.response.venues[1].id;
     var restaurant3 = response.response.venues[2].id;
 
-    restaurantVenueId = [
+    restaurantVenueId3 = [
       restaurant1,
       restaurant2,
       restaurant3
     ];
 
-    getRestaurantPic();
+    getRestaurantPic3();
 
     console.log("Restaurant 1 ID: " + response.response.venues[0].id);
     console.log("Restaurant 2 ID: " + response.response.venues[1].id);
     console.log("Restaurant 3 ID: " + response.response.venues[2].id);
 
-    //Adding information to hotel suggestion cards.
-    //Adding Hotel 1.
+    //Adding information to restaurant suggestion cards.
+    //Adding Restaurant 1.
     $("#restaurant1").append("Restaurant Name: " + response.response.venues[0].name);
     $("#restaurant1Address").append("Restaurant Address: " + response.response.venues[0].location.formattedAddress[0] + ", " + response.response.venues[0].location.formattedAddress[1]);
-    //Adding Hotel 2.
+    //Adding Restaurant 2.
     $("#restaurant2").append("Restaurant Name: " + response.response.venues[1].name);
     $("#restaurant2Address").append("Restaurant Address: " + response.response.venues[1].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
-    //Adding Hotel 3.
+    //Adding Restaurant 3.
     $("#restaurant3").append("Hotel Name: " + response.response.venues[2].name);
-    $("#restaurant3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
+    $("#restaurant3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[2].location.formattedAddress[1]);
   });
 
-  var activityQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + cassieClientId + "&client_secret=" + cassieClientSecret + "&near=denver&query=activity&v=20190415"
+  //function showRestaurantsMoreThanWeek
+  var restaurantQueryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + cassieClientId + "&client_secret=" + cassieClientSecret + "&near=denver&query=restaurant&v=20190415"
 
   $.ajax({
-    url: activityQueryURL,
+    url: restaurantQueryURL,
     method: "GET"
   }).then(function (response) {
-    //Entire object pulled.
-    console.log(response);
-    //Pulling location to be added to location on trip card.
-    console.log(response.response.geocode.feature.displayName);
-    //Pulling hotel IDs for getting pictures for cards.
-    var activity1 = response.response.venues[0].id;
-    var activity2 = response.response.venues[1].id;
-    var activity3 = response.response.venues[2].id;
 
-    activityVenueId = [
-      activity1,
-      activity2,
-      activity3
+    var restaurant1 = response.response.venues[0].id;
+    var restaurant2 = response.response.venues[1].id;
+    var restaurant3 = response.response.venues[2].id;
+    var restaurant4 = response.response.venues[3].id;
+    var restaurant5 = response.response.venues[4].id;
+
+    restaurantVenueId5 = [
+      restaurant1,
+      restaurant2,
+      restaurant3,
+      restaurant4,
+      restaurant5
     ];
 
-    getActivityPic();
+    getRestaurantPic5();
 
-    console.log("Activity 1 ID: " + response.response.venues[0].id);
-    console.log("Activity 2 ID: " + response.response.venues[1].id);
-    console.log("Activity 3 ID: " + response.response.venues[2].id);
+    console.log("Restaurant 1 ID: " + response.response.venues[0].id);
+    console.log("Restaurant 2 ID: " + response.response.venues[1].id);
+    console.log("Restaurant 3 ID: " + response.response.venues[2].id);
+    console.log("Restaurant 4 ID: " + response.response.venues[3].id);
+    console.log("Restaurant 5 ID: " + response.response.venues[4].id);
 
-    //Adding information to activity suggestion cards.
-    //Adding Activity 1.
-    $("#activity1").append("Activity Name: " + response.response.venues[0].name);
-    $("#activity1Address").append("Activity Address: " + response.response.venues[0].location.formattedAddress[0] + ", " + response.response.venues[0].location.formattedAddress[1]);
-    //Adding Activity 2.
-    $("#activity2").append("Activity Name: " + response.response.venues[1].name);
-    $("#activity2Address").append("Activity Address: " + response.response.venues[1].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
-    //Adding Activity 3.
-    $("#activity3").append("Activity Name: " + response.response.venues[2].name);
-    $("#activity3Address").append("Activity Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
+    //Adding information to restaurant suggestion cards.
+    //Adding Restaurant 1.
+    $("#restaurant1").append("Restaurant Name: " + response.response.venues[0].name);
+    $("#restaurant1Address").append("Restaurant Address: " + response.response.venues[0].location.formattedAddress[0] + ", " + response.response.venues[0].location.formattedAddress[1]);
+    //Adding Restaurant 2.
+    $("#restaurant2").append("Restaurant Name: " + response.response.venues[1].name);
+    $("#restaurant2Address").append("Restaurant Address: " + response.response.venues[1].location.formattedAddress[0] + ", " + response.response.venues[1].location.formattedAddress[1]);
+    //Adding Restaurant 3.
+    $("#restaurant3").append("Hotel Name: " + response.response.venues[2].name);
+    $("#restaurant3Address").append("Hotel Address: " + response.response.venues[2].location.formattedAddress[0] + ", " + response.response.venues[2].location.formattedAddress[1]);
+    //Adding Restaurant 4.
+    $("#restaurant4").append("Hotel Name: " + response.response.venues[3].name);
+    $("#restaurant3Address").append("Hotel Address: " + response.response.venues[3].location.formattedAddress[0] + ", " + response.response.venues[3].location.formattedAddress[1]);
+    //Adding Restaurant 5.
+    $("#restaurant5").append("Hotel Name: " + response.response.venues[4].name);
+    $("#restaurant3Address").append("Hotel Address: " + response.response.venues[4].location.formattedAddress[0] + ", " + response.response.venues[4].location.formattedAddress[1]);
   });
 
   //Function that will run to pull photo for each hotel card.
-  function getHotelPic() {
-    for (i = 0; i < hotelVenueId.length; i++) {
+  function getHotelPic3() {
+    for (i = 0; i < hotelVenueId3.length; i++) {
       queryURLExplore = "https://api.foursquare.com/v2/venues/" + hotelVenueId[i] + "?&" + currentAuth;
       $.ajax({
         url: queryURLExplore,
@@ -150,10 +204,26 @@ $(document).ready(function () {
     };
   };
 
+  //Function that will run to pull photo for each hotel card.
+  function getHotelPic5() {
+    for (i = 0; i < hotelVenueId5.length; i++) {
+      queryURLExplore = "https://api.foursquare.com/v2/venues/" + hotelVenueId5[i] + "?&" + currentAuth;
+      $.ajax({
+        url: queryURLExplore,
+        method: "GET"
+      }).then(function (response) {
+        //Pulls photo for location card from object.
+        console.log(response.response.venue.photos.groups[1].items[0].prefix + "200x200" + response.response.venue.photos.groups[1].items[0].suffix);
+        var hotelSrc = response.response.venue.photos.groups[1].items[0].prefix + "200x200" + response.response.venue.photos.groups[1].items[0].suffix;
+        $(`#hotelpic${i}`).attr("src", hotelSrc);
+      });
+    };
+  };
+
   //Function that will run to pull photo for each restaurant card.
-  function getRestaurantPic() {
-    for (i = 0; i < restaurantVenueId.length; i++) {
-      queryURLExplore = "https://api.foursquare.com/v2/venues/" + restaurantVenueId[i] + "?&" + currentAuth;
+  function getRestaurantPic3() {
+    for (i = 0; i < restaurantVenueId3.length; i++) {
+      queryURLExplore = "https://api.foursquare.com/v2/venues/" + restaurantVenueId3[i] + "?&" + currentAuth;
       $.ajax({
         url: queryURLExplore,
         method: "GET"
@@ -167,9 +237,9 @@ $(document).ready(function () {
   };
 
   //Function that will run to pull photo for each restaurant card.
-  function getActivityPic() {
-    for (i = 0; i < activityVenueId.length; i++) {
-      queryURLExplore = "https://api.foursquare.com/v2/venues/" + activityVenueId[i] + "?&" + currentAuth;
+  function getRestaurantPic5() {
+    for (i = 0; i < activityVenueId5.length; i++) {
+      queryURLExplore = "https://api.foursquare.com/v2/venues/" + restaurantVenueId5[i] + "?&" + currentAuth;
       $.ajax({
         url: queryURLExplore,
         method: "GET"
